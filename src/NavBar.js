01756a8ec588bbe83a83c99ae3a91959a95b207d
laith <laithabukhalaf21@gmail.com>
2021-08-12 15:35:37 +0300

    reflix0.2

---
+++ src/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./NaveBar.css";
+
+function NavBar() {
+    
+    return (
+        <div className="nav">
+            <div className="logo">
+                Reflix 0.2
+            </div>
+            
+        </div>
+    )
+}
+
+export default NavBar
