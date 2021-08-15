d89448cfc1851d15c90870180e97eca8c6757ad4
laith <laithabukhalaf21@gmail.com>
2021-08-15 12:47:16 +0300

    reflix0.21

--- src/Row.js
+++ src/Row.js
@@ -4,68 +4,65 @@ import './Row.css';
 import Youtube from 'react-youtube';
 import movieTrailer from 'movie-trailer';
 
-const base_url="https://image.tmdb.org/t/p/original/";
-function Row({title, fetchUrl,isLargeRow}) {
-    const [movies, setMovies] = useState([]);
-    const [trailerUrl, setTrailerUrl] = useState("")
 
-    useEffect(() => {
-        async function fetchData(){
-            const request=await axios.get(fetchUrl);
-            
-            setMovies(request.data.results);
-            return request;
-        }
-        fetchData();
-       
-    }, [fetchUrl]);
+const baseImgUrl = "https://image.tmdb.org/t/p/original";
 
-    const opts = {
-        height: "390",
-        width: "100%",
-        playerVars: {
-            // https://developers.google.com/youtube/player_parameters
-            autoplay: 1,
-          },
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  // Options for react-youtube
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
     }
-    const handleClick = (movie) => {
-        if( trailerUrl ){
-            setTrailerUrl('');
-        }else {
-            movieTrailer(movie?.name || "")
-                .then(url => {
-                    const urlParams = new URLSearchParams(new URL(url).search);
-                    setTrailerUrl(urlParams.get('v'));
-                })
-                .catch((error) => console.log(error))
-        }
-        
+    fetchData();
+  }, [fetchUrl]);
+
+  const handleClick = async (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      let trailerurl = await axios.get(
+        `/movie/${movie.id}/videos?api_key=fb34530271b349314af0de263d16ab5a`
+      );
+      setTrailerUrl(trailerurl.data.results[0]?.key);
     }
-    
+  };
 
-    return (
+  return (
+    <div className="row">
+      <h2>{title}</h2>
 
-            <div className="row">
-                
-            <h2>{title}</h2>
-                <div className="row_posters">
-                    {movies.map(movie=>(
-                        
-                        <img onClick={() => handleClick(movie)}
-                            className={`row_poster ${isLargeRow && "row_posterLarge"}`} key={movie.id}
-                            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path
-                                }`}
-                            alt={movie.name}
-                        />
-                    ))}
-            </div> 
-                {trailerUrl && <Youtube videoId={trailerUrl}opts={opts} />} 
-            </div>
-
-            
-
-
-    )
+      <div className="row_posters">
+        {movies.map(
+          (movie) =>
+            movie.backdrop_path !== null && (
+              <img
+                className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+                src={`${baseImgUrl}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+                key={movie.id}
+                onClick={() => handleClick(movie)}
+              />
+            )
+        )}
+      </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
 }
 
-export default Row
+export default Row;
